e44259d515ace78baee89acddc3825545fd168fc
Chiara Caglieri <c.caglieri@reply.it>
2020-02-04 14:44:25 +0100

    validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ import { User } from '../shared/model/User';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup ;
-  loginError = false;
+  showError = false;
   loading = false;
 
   constructor(private router: Router,private formBuilder: FormBuilder, private userService: UserService) {
@@ -28,15 +28,15 @@ export class LoginComponent implements OnInit {
     this.loading=true;
     this.userService.loadUserDetail(loginData.get("email").value, loginData.get("password").value).subscribe(
       (response) => {
-          this.loginError = false;
+          this.showError = false;
           let json: JSON = response.body;
           this.userService.loggedUser = json['data'];
           this.loading=false;
           this.router.navigateByUrl('/dashboard');
       },
       (error) => {
-        this.loginError = true;
-          this.loading=false;
+        this.showError = true;
+        this.loading=false;
       }
     );
 
